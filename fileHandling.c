8e1adb46969cdd54e67f67b61407c81b1453cf54
GeorgeMGomez <george.gomez04032@gmail.com>
2025-08-05 16:32:57 +0300

    Added View All contacts and search contacts function

--- fileHandling.c
+++ fileHandling.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 void addContact();
+void viewContacts();
+void searchContact();
 
 struct contact {
     char name[50];
@@ -31,6 +34,12 @@ int main(int argc, char *argv[]){
         case 1:
             addContact();
             break;
+        case 2:
+            viewContacts();
+            break;
+        case 3:
+            searchContact();
+            break;
     }
 
     return 0;
@@ -42,22 +51,85 @@ void addContact(){
 
     FILE *fp = fopen("data.txt", "a");
 
+    if(fp==NULL){
+        printf("File is not opened\n");
+        fclose(fp);
+        return;
+    }
+
     printf("Add your name: ");
     scanf("%s", person.name);
-    printf("\n");
-
+    
     printf("Add your phone number: ");
     scanf("%s", person.phone);
-    printf("\n");
-
+    
     printf("Add your email: ");
     scanf("%s", person.email);
 
     system("clear");
 
     printf("---CONTACT DETAILS---\n");
-    printf("Name: %s\n", person.name);
-    printf("Phone number: %s\n", person.phone);
-    printf("Email: %s\n", person.email);
+    printf("Name:%s\n", person.name);
+    printf("Phone number:%s\n", person.phone);
+    printf("Email:%s\n", person.email);
+
+    fprintf(fp, "Name:%s  Phone number:%s  Email:%s\n", person.name, person.phone, person.email);
+    fclose(fp);
+
+    printf("Contact added successfully to the file\n");
     
+}
+
+void viewContacts(){
+    char data[50];
+
+    FILE *fp2 = fopen("data.txt", "r");
+
+    if(fp2==NULL){
+        printf("File is not opened\n");
+        fclose(fp2);
+        return;
+    }
+
+    printf("---VIEW CONTACTS---\n");
+    while(fgets(data, 50, fp2)!=NULL){
+        printf("%s", data);
+    }
+
+    fclose(fp2);
+}
+
+void searchContact(){
+    char searchName[50];
+    char line[100];
+    bool found=false;
+
+    FILE *fp3 = fopen("data.txt", "r");
+
+    if(fp3==NULL){
+        printf("File is not opened\n");
+        fclose(fp3);
+        return;
+    }
+
+    printf("Enter Name: ");
+    scanf("%s", searchName);
+    
+    while(fgets(line, sizeof(line), fp3)!=NULL){
+        char tag[50] = "Name:";
+        strcat(tag, searchName);
+
+        if (strstr(line, tag) != NULL) {
+            printf("---CONTACT FOUND---\n");
+            printf("%s\n", line);
+            found = true;
+            break;  
+        }
+    }
+
+    if(!found){
+        printf("Contact with name %s is not found\n", searchName);
+    }
+
+    fclose(fp3);
 }
